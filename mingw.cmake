f51f5d9051524fd5ad33097ad59b34abec982f6a
bit6tream <megapixel483@gmail.com>
2024-01-13 17:14:57 +0300

    Specify C++ compiler

--- mingw.cmake
+++ mingw.cmake
@@ -3,6 +3,7 @@ set(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
